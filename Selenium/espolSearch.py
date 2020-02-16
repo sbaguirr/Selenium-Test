915669872546af4048aeeee69866be548a3e6ee7
Stefanny Aguirre <sbaguirr@espol.edu.ec>
2020-02-15 23:26:21 -0500

    get career name

--- Selenium/espolSearch.py
+++ Selenium/espolSearch.py
@@ -1,7 +1,6 @@
 import unittest
 import csv
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
 class EspolSearch(unittest.TestCase):
@@ -15,14 +14,15 @@ class EspolSearch(unittest.TestCase):
         driver.get("http://www.espol.edu.ec/es/educacion/grado/catalogo")
         faculty_list = driver.find_elements_by_xpath("//div[@id='accordion']/div/div/h4/a/strong")
         ul_list = driver.find_elements_by_xpath("//div[@class='panel-body']/ul[2]")
-        data = [['career_name_en','career_code','faculty_name','link_to_career_curriculum']]
+        data = [['career_name_en', 'career_code', 'faculty_name', 'link_to_career_curriculum']]
         for i in range(len(faculty_list)):
             faculty_name = faculty_list[i].text.split("\n")
             li_list = ul_list[i].find_elements_by_xpath(".//li")
             for li in li_list:
+                data_li = li.get_attribute("textContent").replace(')', "(").split('(')
+                career_name = data_li[0]
                 career_link = li.find_element_by_xpath(".//a").get_attribute("href")
-                career_code = li.find_element_by_xpath(".//span").get_attribute("textContent")
-                career_name = li.text
+                career_code = data_li[2]
                 data.append([career_name, career_code, faculty_name[1], career_link])
 
         file = open('data.csv', 'w')
